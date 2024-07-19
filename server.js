faf64a4097c7b32b579a6fb16901101481809779
Lena | Dunkel <7890309+DarkView@users.noreply.github.com>
2024-07-19 23:56:07 +0200

    Add simple express host

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(__dirname + "/dist/spectra-frontend/browser"));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/spectra-frontend/browser/index.html"));
+});
+
+app.listen(4200);
+console.log(`SpectraFrontend running on: port ${4200}`);
