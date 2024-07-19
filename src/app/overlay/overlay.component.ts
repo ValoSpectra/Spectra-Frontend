2b2963d582e746756a98dbc4e8c9a4401c4cb9d7
Tiranthine <22277108+Tiranthine@users.noreply.github.com>
2024-07-20 00:10:51 +0200

    SocketService and 0.1.0

--- src/app/overlay/overlay.component.ts
+++ src/app/overlay/overlay.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { TrackerComponent } from '../tracker/tracker.component';
+import { ActivatedRoute } from '@angular/router';
+import { SocketService } from '../services/SocketService';
 
 
 @Component({
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './overlay.component.html',
   styleUrls: ['./overlay.component.scss']
 })
-export class OverlayComponent implements OnInit {
+export class OverlayComponent implements OnInit, AfterViewInit {
+
+  @ViewChild(TrackerComponent) trackerComponent!: TrackerComponent;
+  groupCode: string = "UNKNOWN";
+  socketService!: SocketService;
+
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(params => {
+      this.groupCode = params['groupCode'].toUpperCase();
+      console.log(`Requested group code is ${this.groupCode}`);
+    });
+  }
 
   ngOnInit(): void {
-    
+    const siteUrl = window.location.hostname;
+    this.socketService = new SocketService(`http://${siteUrl}:5200`, this.groupCode);
+  }
+
+  ngAfterViewInit(): void {
+    this.socketService.subscribe((data: any) => {this.trackerComponent.updateMatch(data)});
   }
 
 }
--- src/app/testing/testing.component.ts
+++ src/app/testing/testing.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { TrackerComponent } from '../tracker/tracker.component';
+import { SocketService } from '../services/SocketService';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-testing',
   templateUrl: './testing.component.html',
   styleUrls: ['./testing.component.scss']
 })
-export class TestingComponent implements OnInit {
+export class TestingComponent implements OnInit, AfterViewInit {
+
+  @ViewChild(TrackerComponent) trackerComponent!: TrackerComponent;
+  groupCode: string = "UNKNOWN";
+  socketService!: SocketService;
+
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(params => {
+      this.groupCode = params['groupCode'].toUpperCase();
+      console.log(`Requested group code is ${this.groupCode}`);
+    });
+  }
 
   ngOnInit(): void {
-    console.log("Testing started");
-    
+    const siteUrl = window.location.hostname;
+    this.socketService = new SocketService(`http://${siteUrl}:5200`, this.groupCode);
+  }
+
+  ngAfterViewInit(): void {
+    this.socketService.subscribe((data: any) => {this.trackerComponent.updateMatch(data)});
   }
 
 }
