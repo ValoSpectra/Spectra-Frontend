630fbf998dafb4efe3d5f249d598ea80920cd8d3
Lena | Dunkel <7890309+DarkView@users.noreply.github.com>
2024-11-01 19:20:45 +0100

    Add status endpoint

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require("path");
 
 app.use(express.static(__dirname + "/dist/spectra-frontend/browser"));
 
+app.get("/status", function (req, res) {
+  res.json({ status: "UP" });
+});
+
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/spectra-frontend/browser/index.html"));
 });
