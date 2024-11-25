3b401d2184415511c18744a90a18ea4ed0000a7a
Jay <jay@projectchaos.ch>
2024-11-25 20:51:48 +0100

    added support for rotating sponsor images

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,3 +1,7 @@
 export class Config {
   serverEndpoint!: string;
+  sponsorImageUrls!: string[];
+  sponsorImageRotateSpeed!: number; // in milliseconds
+  attackerColor!: string;
+  defenderColor!: string;
 }
--- src/app/topscore/topinfo/topinfo.component.ts
+++ src/app/topscore/topinfo/topinfo.component.ts
@@ -1,8 +1,42 @@
 import { Component } from "@angular/core";
+import { Config } from "../../shared/config";
+import { animate, style, transition, trigger } from "@angular/animations";
 
 @Component({
   selector: "app-topinfo",
   templateUrl: "./topinfo.component.html",
   styleUrls: ["./topinfo.component.scss"],
+  animations: [
+    trigger("fadeInOut", [
+      transition(":enter", [
+        style({ opacity: 0 }),
+        animate("1s", style({ opacity: 1 })),
+      ]),
+      transition(":leave", [animate("1s", style({ opacity: 0 }))]),
+    ]),
+  ],
 })
-export class TopinfoComponent {}
+export class TopinfoComponent {
+  sponsorsAvailable = false;
+  sponsorImages: string[] = [];
+  currentSponsorIndex = 0;
+
+  constructor(private config: Config) { }
+
+  ngOnInit() {
+    this.sponsorsAvailable = this.config.sponsorImageUrls.length > 0;
+    if (this.sponsorsAvailable) {
+      this.sponsorImages = this.config.sponsorImageUrls;
+      this.currentSponsorIndex = 0;
+      setInterval(
+        () => this.nextSponsor(),
+        this.config.sponsorImageRotateSpeed,
+      );
+    }
+  }
+
+  nextSponsor() {
+    this.currentSponsorIndex =
+      (this.currentSponsorIndex + 1) % this.config.sponsorImageUrls.length;
+  }
+}
