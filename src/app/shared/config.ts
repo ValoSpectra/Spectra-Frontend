980bfe0401edd7f78d2cec5531aa0f0ab74d4aee
Dunkel <7890309+DarkView@users.noreply.github.com>
2025-01-10 19:28:47 +0100

    Get the colors a bit closer to the current colors

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -2,11 +2,13 @@ export class Config {
   serverEndpoint = "http://localhost:5200";
   sponsorImageUrls: string[] = [];
   sponsorImageRotateSpeed = 5000; // in milliseconds
+
   attackerColorPrimary = "#b82e3c";
-  attackerColorSecondary = "#a30010";
+  attackerColorSecondary = "#ff4557";
   attackerColorShieldCurrency = "#ff838f";
-  defenderColorPrimary = "#1e8a61";
-  defenderColorSecondary = "#138c69";
+
+  defenderColorPrimary = "#25ac79";
+  defenderColorSecondary = "#61eab6";
   defenderColorShieldCurrency = "#61eab6";
 
   public constructor(init?: Partial<Config>) {
