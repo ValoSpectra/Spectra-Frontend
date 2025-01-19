5db423a7c3569bda87c4c0751adb4eb1f4a6be89
b4iterdev <thaimeo200389@gmail.com>
2025-01-19 11:21:20 +0700

    run lint and format

--- src/app/timeout/timeout.component.ts
+++ src/app/timeout/timeout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, Input } from "@angular/core";
+import { Component, OnInit, ViewChild, AfterViewInit } from "@angular/core";
 import { TrackerComponent } from "../tracker/tracker.component";
 import { SocketService } from "../services/SocketService";
 import { ActivatedRoute } from "@angular/router";
@@ -17,51 +17,67 @@ import { trigger, transition, style, animate } from "@angular/animations";
     ]),
   ],
 })
-
-export class TimeoutComponent implements OnInit {
-    @ViewChild(TrackerComponent) trackerComponent!: TrackerComponent;
-    @Input() match!: any;
-    team: string = this.match.timeout.team || "tech";
-    time: number = this.match.timeout.time || 30;
-    maxtimeout: number = this.match.timeout.maxtimeout || 2;
-    teamLeft: number = this.match.timeout.teamLeft || 0;
-    teamRight: number = this.match.timeout.teamRight || 0;
-    groupCode = "UNKNOWN";
-    socketService!: SocketService;
-    constructor(
-        private route: ActivatedRoute,
-        private config: Config,
-      ) {
-        this.route.queryParams.subscribe((params) => {
-          this.groupCode = params["groupCode"]?.toUpperCase() || "UNKNOWN";
-          console.log(`Requested group code is ${this.groupCode}`);
-          this.team = params["team"]! || "tech";
-          this.time = params["time"]! || 30;
-          this.maxtimeout = params["maxtimeout"]! || 2;
-          this.teamLeft = params["teamLeft"]! || 0;
-          this.teamRight = params["teamRight"]! || 0;
-          console.log(`Timeout called by ${this.team}`);
-          console.log(`Timeout with ${this.time} seconds`);
-          console.log(`Timeout with ${this.maxtimeout} max timeouts`);
-          console.log(`Timeout with ${this.teamLeft} timeout(s) left for left team`);
-          console.log(`Timeout with ${this.teamRight} timeouts(s) left for right team`);
-        });
-      }
-    timeoutUrl: string = "";
-    tournamentBackgroundUrl: string =
-    this.match?.tournamentBackgroundUrl && this.match.tournamentBackgroundUrl !== ""
-      ? this.match.tournamentBackgroundUrl
-      : "../../assets/misc/endround-bg.webp";
-    
-    ngOnInit(): void {
-        this.socketService = SocketService.getInstance(this.config.serverEndpoint, this.groupCode);
-        this.preloadImage(this.tournamentBackgroundUrl);
-    }
-    private preloadImage(url: string): void {
-        const img = new Image();
-        img.src = url;
-    }
-    setTournamentBackgroundImage(): string {
-        return `url(${this.tournamentBackgroundUrl})`;
-    }
+export class TimeoutComponent implements OnInit, AfterViewInit {
+  @ViewChild(TrackerComponent) trackerComponent!: TrackerComponent;
+  groupCode = "UNKNOWN";
+  socketService!: SocketService;
+  match: any;
+  timeout: any;
+  timeoutUrl!: string;
+  tournamentBackgroundUrl!: string;
+  constructor(
+    private route: ActivatedRoute,
+    private config: Config,
+  ) {
+    this.route.queryParams.subscribe((params) => {
+      this.groupCode = params["groupCode"]?.toUpperCase() || "UNKNOWN";
+      console.log(`Requested group code is ${this.groupCode}`);
+    });
+  }
+  ngOnInit(): void {
+    this.match = {
+      groupCode: "A",
+      isRanked: false,
+      isRunning: true,
+      roundNumber: 0,
+      roundPhase: "combat",
+      teams: [{ players: [] }, { players: [] }],
+      spikeState: { planted: false },
+      map: "Ascent",
+      tools: {
+        timeout: {
+          team: "tech",
+          time: 30,
+          maxtimeout: 2,
+          teamLeft: 0,
+          teamRight: 0,
+        },
+      },
+    };
+    this.timeout = this.match.tools.timeout;
+    this.socketService = SocketService.getInstance(this.config.serverEndpoint, this.groupCode);
+    this.tournamentBackgroundUrl =
+      this.match?.tournamentBackgroundUrl && this.match.tournamentBackgroundUrl !== ""
+        ? this.match.tournamentBackgroundUrl
+        : "../../assets/misc/endround-bg.webp";
+    this.preloadImage(this.tournamentBackgroundUrl);
+  }
+  ngAfterViewInit(): void {
+    this.socketService.subscribe((data: any) => {
+      this.updateTimeout(data);
+    });
+  }
+  public updateTimeout(data: any) {
+    delete data.eventNumber;
+    delete data.replayLog;
+    this.match = data;
+    this.timeout = this.match.tools.timeout;
+  }
+  private preloadImage(url: string): void {
+    const img = new Image();
+    img.src = url;
+  }
+  setTournamentBackgroundImage(): string {
+    return `url(${this.tournamentBackgroundUrl})`;
+  }
 }
