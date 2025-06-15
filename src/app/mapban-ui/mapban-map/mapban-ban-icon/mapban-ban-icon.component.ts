75baf3174f2f1dcdc6d97889d8f9d11547fd4f46
Dunkel <7890309+DarkView@users.noreply.github.com>
2025-06-15 14:11:15 +0200

    Ban SVG animation

--- src/app/mapban-ui/mapban-map/mapban-ban-icon/mapban-ban-icon.component.ts
+++ src/app/mapban-ui/mapban-map/mapban-ban-icon/mapban-ban-icon.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { AfterViewInit, Component } from "@angular/core";
+import { animate, svg, eases } from "animejs";
 
 @Component({
   selector: "app-mapban-ban-icon",
@@ -7,4 +8,17 @@ import { Component } from "@angular/core";
   templateUrl: "./mapban-ban-icon.component.html",
   styleUrl: "./mapban-ban-icon.component.css",
 })
-export class MapbanBanIconComponent {}
+export class MapbanBanIconComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    //#region Ban SVG
+    animate(svg.createDrawable("#banIcon"), {
+      draw: ["0 0", "0 1"],
+      ease: eases.inOutQuad,
+      duration: 1500,
+      delay: 500,
+      autoplay: true,
+      loop: false,
+    });
+    //#endregion
+  }
+}
