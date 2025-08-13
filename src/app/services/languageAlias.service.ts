52ebdbc2ff9eb66a224ceb91e24ff89e5f0acee5
Lacy <imlacy.ig@gmail.com>
2025-08-13 07:50:57 +0200

    Added Chinese,Simplified and Traditional (by HsuDDfps)

--- src/app/services/languageAlias.service.ts
+++ src/app/services/languageAlias.service.ts
@@ -1,6 +1,7 @@
 export class LanguageAliasService {
   static LanguageAliases: Record<string, string> = {
     jp: "ja",
+    zh_HANS: "zh_CN",
   };
 
   public static resolveLanguageAlias(alias: string) {
