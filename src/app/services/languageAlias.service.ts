b66b9006141a79ab6581767030e5cf8168cfdd9d
Lacy <imlacy.ig@gmail.com>
2025-08-14 22:09:45 +0200

    Added tactical timeout teamName and mapinfo map as variable in locales (#25)
    * Added tactical timeout teamName and mapinfo map as variable in locales
    
    * Added placeholder for topscore and endround
    
    * Format
    
    * Added Chinese,Simplified and Traditional (by HsuDDfps)
    
    * Patch on languageAlias.service.ts

--- src/app/services/languageAlias.service.ts
+++ src/app/services/languageAlias.service.ts
@@ -1,6 +1,7 @@
 export class LanguageAliasService {
   static LanguageAliases: Record<string, string> = {
     jp: "ja",
+    zh_CN: "zh_HANS",
   };
 
   public static resolveLanguageAlias(alias: string) {
