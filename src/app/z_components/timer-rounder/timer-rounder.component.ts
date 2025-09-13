6f73a9486854b80e71e5ef92ad471e1b462e20c5
Dunkel <7890309+DarkView@users.noreply.github.com>
2025-09-14 00:17:31 +0200

    Run formatter

--- src/app/z_components/timer-rounder/timer-rounder.component.ts
+++ src/app/z_components/timer-rounder/timer-rounder.component.ts
@@ -1,11 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-timer-rounder',
+  selector: "app-timer-rounder",
   imports: [],
-  templateUrl: './timer-rounder.component.html',
-  styleUrl: './timer-rounder.component.css'
+  templateUrl: "./timer-rounder.component.html",
+  styleUrl: "./timer-rounder.component.css",
 })
-export class TimerRounderComponent {
-
-}
+export class TimerRounderComponent {}
